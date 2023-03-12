96c17904e1f9ac504ee4369c264f955229aadfc6
YUriy N. Tarasenko <ytarasenko@inno.tech>
2023-03-12 23:21:20 +0300

    deploy

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.tsx", // точка входа, о которой говорилось ранее.
-  mode: "development",
+  mode: "production",
   module: {
     rules: [
       {
