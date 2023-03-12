d94c9d1cac1e5a631b9f5a5a4c93615223d67be3
YUriy N. Tarasenko <ytarasenko@inno.tech>
2023-03-12 23:46:54 +0300

    Deploy"

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,7 @@ module.exports = {
     extensions: [".tsx", ".ts", ".js"],
   },
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "public"),
     filename: "bundle.js", // выходной бандл
   },
   devServer: {
